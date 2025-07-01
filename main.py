87809e404f6d2712f59c01436d3aa58fe65fee77
menaceinc <john@menaceinc.com>
2025-07-01 03:37:11 +0100

    All drawing, no collision on shots yet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -8,7 +13,28 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    asteroids = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
+
+    Shot.containers = (shots, drawable, updateable)
+
+    Player.containers = (drawable, updateable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    Asteroid.containers = (asteroids, drawable, updateable)
+    AsteroidField.containers = (updateable)
+
+    asteroid_field = AsteroidField()
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +42,21 @@ def main():
                 return
     
         screen.fill("black")
+
+        updateable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit(0)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
